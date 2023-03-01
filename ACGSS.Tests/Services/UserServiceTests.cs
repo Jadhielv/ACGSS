f81be95c3cadd87d9838bbc98ec109ea43883b08
Jadhiel Vélez <jadhielv@gmail.com>
2023-03-01 18:01:45 -0400

    refactor: defining Tests layer

---
+++ ACGSS.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,187 @@
+﻿using ACGSS.Application.Services;
+using ACGSS.Domain.DTOs;
+using ACGSS.Domain.Entities;
+using ACGSS.Domain.Exceptions;
+using ACGSS.Domain.Repositories;
+using AutoMapper;
+using Moq;
+using System.Linq.Expressions;
+
+namespace ACGSS.Tests.Services
+{
+    public class UserServiceTests
+    {
+        [Test]
+        public void UpdateUser_UserNotFound_Should_ThrowsConflictException()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            var user = new UserDto
+            {
+                Id = 1,
+                FirstName = "Karl Matti",
+                LastName = "Jablonski Karttunen",
+                Email = "test@gmail.com"
+            };
+
+            var service = new UserService(unitOfWorkMock.Object, null);
+
+            userRepositoryMock.Setup(x => x.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(false);
+
+            unitOfWorkMock.Setup(x => x.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act => Assert
+            Assert.ThrowsAsync<ConflictException>(() => service.UpdateUser(user), "The user doesn't exist.");
+        }
+
+        [Test]
+        public async Task UpdateUser_UserFound_Should_UpdateUser()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var user = new UserDto
+            {
+                Id = 1,
+                FirstName = "Karl Matti",
+                LastName = "Jablonski Karttunen",
+                Email = "test@gmail.com"
+            };
+
+            var service = new UserService(unitOfWorkMock.Object, mapperMock.Object);
+
+            userRepositoryMock.Setup(v => v.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(true);
+
+            unitOfWorkMock.Setup(v => v.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act
+            await service.UpdateUser(user);
+
+            // Assert
+            userRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<User>()));
+            unitOfWorkMock.Verify(x => x.SaveChangesAsync());
+        }
+
+        [Test]
+        public void GetUser_UserNotFound_Should_ThrowsConflictException()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            var service = new UserService(unitOfWorkMock.Object, null);
+
+            userRepositoryMock.Setup(x => x.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(false);
+
+            unitOfWorkMock.Setup(x => x.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act => Assert
+            Assert.ThrowsAsync<ConflictException>(() => service.GetUser(1), "The user doesn't exist.");
+        }
+
+        [Test]
+        public async Task GetUser_UserFound_Should_ReturnsUser()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            const int userId = 1;
+
+            var user = new User
+            {
+                Id = userId,
+                FirstName = "Karl Matti",
+                LastName = "Jablonski Karttunen",
+                Email = "test@gmail.com"
+            };
+
+            var userDto = new UserDto();
+
+            var service = new UserService(unitOfWorkMock.Object, mapperMock.Object);
+
+            userRepositoryMock.Setup(x => x.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(true);
+
+            userRepositoryMock.Setup(x => x.GetFirstAsync(x => x.Id == userId))
+                .ReturnsAsync(user);
+
+            mapperMock.Setup(x => x.Map<UserDto>(user))
+                .Returns(userDto);
+
+            unitOfWorkMock.Setup(v => v.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act 
+            var result = await service.GetUser(userId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(userDto));
+        }
+
+        [Test]
+        public void DeleteUser_UserNotFound_Should_ThrowsConflictException()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            var service = new UserService(unitOfWorkMock.Object, null);
+
+            userRepositoryMock.Setup(v => v.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(false);
+
+            unitOfWorkMock.Setup(v => v.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act => Assert
+            Assert.ThrowsAsync<ConflictException>(() => service.DeleteUser(1), "The user doesn't exist.");
+        }
+
+        [Test]
+        public async Task DeleteUser_UserFound_Should_DeleteUser()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            const int userId = 1;
+
+            var user = new User
+            {
+                Id = userId,
+                FirstName = "Karl Matti",
+                LastName = "Jablonski Karttunen",
+                Email = "test@gmail.com"
+            };
+
+            var service = new UserService(unitOfWorkMock.Object, mapperMock.Object);
+
+            userRepositoryMock.Setup(v => v.AnyAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                .ReturnsAsync(true);
+
+            userRepositoryMock.Setup(x => x.GetFirstAsync(x => x.Id == userId))
+                .ReturnsAsync(user);
+
+            unitOfWorkMock.Setup(v => v.UserRepository)
+                .Returns(userRepositoryMock.Object);
+
+            // Act 
+            await service.DeleteUser(userId);
+
+            // Assert
+            userRepositoryMock.Verify(x => x.DeleteAsync(user));
+            unitOfWorkMock.Verify(x => x.SaveChangesAsync());
+        }
+    }
+}
---
+++ ACGSS.Tests/Validators/UserValidatorTests.cs
@@ -0,0 +1,98 @@
+﻿using ACGSS.Domain.DTOs;
+using ACGSS.Infrastructure.Validators;
+using FluentValidation.TestHelper;
+
+namespace ACGSS.Tests.Validators
+{
+    [TestFixture]
+    public class UserValidatorTests
+    {
+        private UserValidator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new UserValidator();
+        }
+
+        [Test]
+        public void Should_have_error_when_FirstName_is_null()
+        {
+            var model = new UserDto { FirstName = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(p => p.FirstName);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_FirstName_is_specified()
+        {
+            var model = new UserDto { FirstName = "Karl Matti" };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(p => p.FirstName);
+        }
+
+        [Test]
+        public void Should_have_error_when_LastName_is_null()
+        {
+            var model = new UserDto { LastName = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(p => p.LastName);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_LastName_is_specified()
+        {
+            var model = new UserDto { LastName = "Jablonski Karttunen" };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(p => p.LastName);
+        }
+
+        [Test]
+        public void Should_have_error_when_Email_is_null()
+        {
+            var model = new UserDto { Email = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(p => p.Email);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_Email_is_specified()
+        {
+            var model = new UserDto { Email = "jadhielv@gmail.com" };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(p => p.Email);
+        }
+
+        [Test]
+        public void Should_have_error_when_CreatedDate_is_null()
+        {
+            var model = new UserDto { CreatedDate = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(p => p.CreatedDate);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_CreatedDate_is_specified()
+        {
+            var model = new UserDto { CreatedDate = DateTime.Now };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(p => p.CreatedDate);
+        }
+
+        [Test]
+        public void Should_have_error_when_ModifiedDate_is_null()
+        {
+            var model = new UserDto { ModifiedDate = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(p => p.ModifiedDate);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_ModifiedDate_is_specified()
+        {
+            var model = new UserDto { ModifiedDate = DateTime.Now };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(p => p.ModifiedDate);
+        }
+    }
+}
